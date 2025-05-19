6871cad2a83019b9c863baef642f2da254b7b79d
Bartosz <br.gruca@gmail.com>
2025-05-19 18:56:15 +0200

    feat: update setup_capture function to handle video and image modes

--- utils/video.py
+++ utils/video.py
@@ -1,10 +1,19 @@
 import cv2
 
 
-def setup_capture(mode, video_path):
-    if mode == "file":
-        print(f"Otwieranie pliku wideo: {video_path}")
-        cap = cv2.VideoCapture(video_path)
+def setup_capture(mode, file_path):
+    if mode == "video":
+        print(f"Otwieranie pliku wideo: {file_path}")
+        cap = cv2.VideoCapture(file_path)
+        if not cap.isOpened():
+            raise RuntimeError("Nie udało się otworzyć pliku wideo!")
+        return cap
+    elif mode == "image":
+        print(f"Otwieranie zdjęcia: {file_path}")
+        frame = cv2.imread(file_path)
+        if frame is None:
+            raise RuntimeError("Nie udało się wczytać zdjęcia!")
+        return frame
     else:
         print("Uruchamianie kamery na żywo")
         cap = cv2.VideoCapture(0)
